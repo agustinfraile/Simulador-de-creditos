d7905289ab798d3abbe5d27707bb3b202c4ce3ce
Agustin Fraile <agustinfraile98@gmail.com>
2021-12-19 21:09:16 -0300

    agregando api del dolar

---
+++ 2da_entrega/JS/API.js
@@ -0,0 +1,23 @@
+let dolares = [];
+let divDolares = document.getElementById("div-dolares");
+
+// cargo la api del dolar a peso argentino
+fetch("https://criptoya.com/api/dolar")
+.then(promesa => promesa.json())
+
+// paso de objeto a array, lo guardo en una variable y lo muestro
+.then(datos => {
+    let usdArray = Object.entries(datos);
+    
+    // recorro el array de dolares
+    usdArray.forEach(dolar => {
+        // muestro en el html el array de dolares
+        if (dolar[0] != "time") {
+            divDolares.innerHTML += `    
+                <p id="${dolar[0]}">  ${dolar[0]}: $${dolar[1]}  </p>
+            ` 
+            dolares.push({tipo: dolar[0], valor: dolar[1]})
+        }
+    })
+})
+.catch(error => console.log(error))
